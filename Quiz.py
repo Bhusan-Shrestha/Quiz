8b5c6b3a36de254dbc8066b6babb592c3f79e883
Bhusan-Shrestha <125342066+Bhusan-Shrestha@users.noreply.github.com>
2024-02-01 08:41:02 +0545

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,67 @@
+questions = [
+    "What is the capital of France?",
+    "Which planet is known as the 'Red Planet'?",
+    " In which year did the Titanic sink?",
+    "Who wrote 'Romeo and Juliet'?",
+    "What is the largest mammal in the world?",
+    "What is the chemical symbol for gold?",
+    "Who painted the Mona Lisa?",
+    "What is the powerhouse of the cell?",
+    "Which country is known as the 'Land of the Rising Sun'?",
+    "Who is the author of the Harry Potter series?",
+    
+]
+
+answers = [
+    "Paris",
+    "Mars",
+    "1912",
+    "William Shakespeare",
+    "Blue Whale",
+    "Au",
+    "Leonardo da Vinci",
+    "Mitochondria",
+    "Japan",
+    "J.K. Rowling"
+]
+
+options = [
+    "A. Berlin \nB. Paris \nC. Rome \nD. Madrid",
+    "A. Venus \nB. Mars \nC. Jupiter \nD. Saturn",
+    "A. 1905 \nB. 1912 \nC. 1920 \nD. 1931",
+    "A. Charles Dickens \nB. William Shakespeare \nC. Jane Austen \nD. Mark Twain",
+    "A. Elephant \nB. Blue Whale \nC. Giraffe \nD. Gorilla",
+    "A. Au \nB. Ag \nC. Fe \nD. Cu",
+    "A. Vincent van Gogh \nB. Leonardo da Vinci \nC. Pablo Picasso \nD. Michelangelo",
+    "A. Nucleus \nB. Mitochondria \nC. Ribosome \nD. Endoplasmic reticulum",
+    "A. China \nB. Japan \nC. South Korea \nD. Thailand",
+    "A. J.K. Rowling \nB. George R.R. Martin \nC. Stephen King \nD. J.R.R. Tolkien",
+]
+
+# main function
+def main():
+    score: int = 0
+    # length = questions.length()
+    print("Welcome to quiz game!\n")
+    input("Press Enter to start")
+
+    # Loop over all questions
+    for a in range(len(questions)):
+        print(f"\nQuestion {a + 1}: {questions[a]}")
+        print(f"\nOptions : \n{options[a]}")
+        answer = input("\nYour answer: ").lower()
+
+        if answer == answers[a].lower():
+            print("\nYour answer was correct!")
+            score += 1
+            
+        else:
+            print("Your answer was incorrect!")
+            print("Answer is "+answers[a])
+            
+    print("Game is over:")
+
+    print(f"Your score is: {score}/{len(questions)}")
+    print("Thanks for playing my quiz game")
+# calling main function
+main()
