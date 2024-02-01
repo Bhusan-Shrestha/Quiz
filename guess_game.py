05ee2d0f053ea8dda402a66b24745a9484a5d9d6
Bhusan-Shrestha <125342066+Bhusan-Shrestha@users.noreply.github.com>
2024-02-01 08:42:30 +0545

    Add files via upload

---
+++ guess_game.py
@@ -0,0 +1,18 @@
+import random
+guess = random.randint(1,10)
+
+guessed = 1
+for i in range(1, 4):
+    num=int(input("Guess a number between 1 to 10: "))
+    if num == guess:
+        print("number found is {} in {} guesses".format(num,guessed))
+        break
+    
+    elif guessed == 3:
+        print("Out of moves")
+        print("The correct number is:", guess)
+    
+    else:
+        guessed += 1
+        print("wrong guess")
+        
